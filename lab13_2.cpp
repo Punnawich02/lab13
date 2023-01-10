4fc68b7a010be2bfe58addd8dad268dd29021317
PunnawichMai <punnawich_dachin@cmu.ac.th>
2023-01-10 15:52:24 +0700

    finished

--- lab13_2.cpp
+++ lab13_2.cpp
@@ -1,9 +1,17 @@
 #include<iostream>
 using namespace std;
 
+double gcd(double,double);
+
 int main(){
     int x, y;
     cin >> x >> y;
     cout << gcd(x,y);
     return 0; 
+}
+
+double gcd(double a ,double b)
+{
+    if(a%b == 0) return b;
+    else return gcd(b,a%b);
 }
--- lab13_4.cpp
+++ lab13_4.cpp
@@ -2,6 +2,9 @@
 
 using namespace std;
 
+template <typename M>
+void mySwap(M &,M &);
+
 int main(){
 	int x, y;
 	string a, b;
@@ -30,3 +33,10 @@ int main(){
 	return 0;
 }
 
+template <typename currentType>
+	void mySwap(currentType &x ,currentType &y){
+		currentType keep;
+		keep = x;
+		x = y;
+		y = keep;
+	}
